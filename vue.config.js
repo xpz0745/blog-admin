40b1172e76aa881200e218d4e41a1018106f6319
xpz0745 <313903714@qq.com>
2021-09-17 10:55:41 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ const defaultSettings = require('./src/settings.js')
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
-
+   
 const name = defaultSettings.title || 'vue Element Admin' // page title
 
 // If your port is set to 80,
